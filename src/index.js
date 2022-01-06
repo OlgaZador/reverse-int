0c38311d42347ec6abb7e8d74783f02f5ffbd11a
OlgaZador <olgastarikovabel.ru>
2022-01-06 16:33:16 +0300

    passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
   
+let str = n.toString();
+
+let str_reverse = str. split("").reverse().join("");
+
+let number_reverse = parseInt(str_reverse);
+
+return number_reverse;
+
 }
